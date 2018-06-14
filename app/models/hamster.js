7e92d33cbf94af0f63e782b9131d7780963a18d7
Max Konin <maxim21214@gmail.com>
2018-06-14 21:07:29 +0700

    cute coef

--- app/models/hamster.js
+++ app/models/hamster.js
@@ -1,4 +1,5 @@
 import DS from 'ember-data';
+import { computed } from '@ember/object';
 
 export default DS.Model.extend({
   weight: DS.attr('number'),
@@ -9,5 +10,16 @@ export default DS.Model.extend({
   avatarUrl: DS.attr('string'),
   state: DS.attr('string'),
 
-  slaves: DS.hasMany('human')
+  slaves: DS.hasMany('human'),
+
+  density: computed('weight', 'height', 'width', function () {
+    return this.weight / (this.height * this.width)
+  }),
+  cuteCoef: computed('density', 'fluffiness', function () {
+    const { density, fluffiness, weight} = this;
+    return (fluffiness * fluffiness *  weight) / Math.sqrt(density);
+  }),
+  normalizedCuteCoef: computed('cuteCoef', function () {
+    return (this.cuteCoef / (100 * 100 * 5));
+  }),
 });
--- tests/unit/models/hamster-test.js
+++ tests/unit/models/hamster-test.js
@@ -1,17 +1,37 @@
 import { expect } from 'chai';
-import { describe, it } from 'mocha';
+import { describe, it, context } from 'mocha';
 import { setupModelTest } from 'ember-mocha';
 
 describe('Unit | Model | hamster', function() {
   setupModelTest('hamster', {
     // Specify the other units that are required for this test.
-    needs: []
+    needs: ['model:human']
   });
 
-  // Replace this with your real tests.
-  it('exists', function() {
-    let model = this.subject();
-    // var store = this.store();
-    expect(model).to.be.ok;
+  describe('#density', function () {
+    it("returns cat's density", function () {
+      const model = this.subject({weight: 2, height: 10, width: 10});
+      expect(model.get('density')).to.eq(0.02);
+    });
+  });
+  describe('#cuteCoef', function () {
+    it('returns interpolated cute coef', function () {
+      const model = this.subject({weight: 1, height: 1, width: 1, fluffiness: 1});
+      expect(model.get('cuteCoef')).to.eq(1);
+    });
+  });
+  describe('#normalizedCuteCoef', function () {
+    context('not max or min', function () {
+      it('returns normalized cute coef', function () {
+        const model = this.subject({weight: 1, height: 10, width: 10, fluffiness: 50});
+        expect(model.get('normalizedCuteCoef')).to.eq(0.5);
+      });
+    });
+    context('max cute', function () {
+      it('returns normalized cute coef', function () {
+        const model = this.subject({weight: 25, height: 100, width: 100, fluffiness: 100});
+        expect(model.get('normalizedCuteCoef')).to.eq(100);
+      });
+    });
   });
 });
